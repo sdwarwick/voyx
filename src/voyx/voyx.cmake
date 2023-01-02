6bdf963233c29377d85429dd10851b7a814597c7
Jürgen Hock <juergen.hock@jurihock.de>
2023-01-02 13:54:19 +0100

    Upgrade threadpool to 3.3.0

--- src/voyx/voyx.cmake
+++ src/voyx/voyx.cmake
@@ -30,7 +30,7 @@ target_link_libraries(voyx
           readerwriterqueue
           rtaudio
           rtmidi
-          thread_pool)
+          threadpool)
 
 target_compile_features(voyx
   PRIVATE cxx_std_20)
