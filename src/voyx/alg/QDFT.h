6cd54e458de6781eb415703031565bcdc2e115ad
Jürgen Hock <juergen.hock@jurihock.de>
2023-01-04 12:14:13 +0100

    Provide optional windowing

--- src/voyx/alg/QDFT.h
+++ src/voyx/alg/QDFT.h
@@ -20,7 +20,11 @@ class QDFT
 
 public:
 
-  QDFT(const double samplerate, std::pair<double, double> bandwidth, const double resolution = 24, const double latency = 0)
+  QDFT(const double samplerate,
+       const std::pair<double, double> bandwidth,
+       const double resolution = 24,
+       const double latency = 0,
+       const std::optional<std::pair<double, double>> window = std::make_pair(+0.5,-0.5))
   {
     const F pi = F(2) * std::acos(F(-1));
 
@@ -28,34 +32,50 @@ public:
     config.bandwidth = bandwidth;
     config.resolution = resolution;
     config.latency = latency;
+    config.window = window;
     config.quality = std::pow(std::pow(2.0, 1.0 / resolution) - 1.0, -1.0);
     config.size = std::ceil(resolution * std::log2(bandwidth.second / bandwidth.first));
 
     config.frequencies.resize(config.size);
-    data.resize(config.size + 1);
 
     for (size_t i = 0; i < config.size; ++i)
     {
       const double frequency = config.bandwidth.first * std::pow(2.0, i / config.resolution);
-      const double period = std::ceil(config.quality * config.samplerate / frequency);
-      const double weight = 1.0 / period;
 
       config.frequencies[i] = frequency;
-      data[i].period = static_cast<size_t>(period);
-      data[i].weight = weight;
-
-      const double offset = (data.front().period - period)
-        * std::clamp(config.latency * 0.5 + 0.5, 0.0, 1.0);
-
-      data[i].offset = static_cast<size_t>(offset);
-
-      data[i].twiddle = std::polar(F(1), +pi * config.quality / period);
     }
 
-    data.back().twiddle = std::polar(F(1), -pi * config.quality);
+    for (const auto k : { -1, 0, +1 })
+    {
+      auto& kernel = kernels[k];
 
-    buffer.input.resize(data.front().period + 1);
-    buffer.output.resize(config.size);
+      kernel.data.resize(config.size + 1);
+
+      for (size_t i = 0; i < config.size; ++i)
+      {
+        const double period = std::ceil(config.quality * config.samplerate / config.frequencies[i]);
+
+        kernel.data[i].period = static_cast<size_t>(period);
+
+        const double offset = (kernel.data.front().period - period)
+          * std::clamp(config.latency * 0.5 + 0.5, 0.0, 1.0);
+
+        kernel.data[i].offset = static_cast<size_t>(offset);
+
+        const F weight = F(1) / period;
+
+        kernel.data[i].weight = weight;
+
+        const std::complex<F> twiddle = std::polar(F(1), +pi * (config.quality + k) / period);
+
+        kernel.data[i].twiddle = twiddle;
+      }
+
+      kernel.data.back().twiddle = std::polar(F(1), -pi * (config.quality + k));
+
+      kernel.buffer.input.resize(kernel.data.front().period + 1);
+      kernel.buffer.output.resize(config.size);
+    }
   }
 
   size_t size() const
@@ -80,20 +100,64 @@ public:
 
   void qdft(const T sample, voyx::vector<std::complex<F>> dft)
   {
-    std::rotate(buffer.input.begin(), buffer.input.begin() + 1, buffer.input.end());
-
-    buffer.input.back() = sample;
-
-    const std::complex<F> twiddle = data.back().twiddle;
-
-    for (size_t i = 0; i < dft.size(); ++i)
+    const auto core = [&](const int k)
     {
-      const F left = buffer.input[data[i].offset + data[i].period];
-      const F right = buffer.input[data[i].offset];
+      const auto& data = kernels[k].data;
 
-      const std::complex<F> delta = twiddle * left - right;
+      auto& input = kernels[k].buffer.input;
+      auto& output = kernels[k].buffer.output;
 
-      dft[i] = buffer.output[i] = data[i].twiddle * (buffer.output[i] + delta * data[i].weight);
+      std::rotate(input.begin(), input.begin() + 1, input.end());
+
+      input.back() = sample;
+
+      const std::complex<F> fiddle = data.back().twiddle;
+
+      for (size_t i = 0; i < config.size; ++i)
+      {
+        const size_t period = data[i].period;
+        const size_t offset = data[i].offset;
+
+        const F weight = data[i].weight;
+        const std::complex<F> twiddle = data[i].twiddle;
+
+        const F left = input[offset + period];
+        const F right = input[offset];
+
+        const std::complex<F> delta = fiddle * left - right;
+
+        output[i] = twiddle * (output[i] + delta * weight);
+      }
+    };
+
+    if (config.window)
+    {
+      const auto k = { -1, 0, +1 };
+
+      std::for_each(k.begin(), k.end(), core);
+
+      const auto& left = kernels[-1].buffer.output;
+      const auto& middle = kernels[0].buffer.output;
+      const auto& right = kernels[+1].buffer.output;
+
+      const F a = config.window.value().first;
+      const F b = config.window.value().second / 2;
+
+      for (size_t i = 0; i < dft.size(); ++i)
+      {
+        dft[i] = a * middle[i] + b * (left[i] + right[i]);
+      }
+    }
+    else
+    {
+      core(0);
+
+      const auto& middle = kernels[0].buffer.output;
+
+      for (size_t i = 0; i < dft.size(); ++i)
+      {
+        dft[i] = middle[i];
+      }
     }
   }
 
@@ -107,6 +171,8 @@ public:
 
   T iqdft(const voyx::vector<std::complex<F>> dft)
   {
+    const auto& data = kernels[0].data;
+
     F sample = F(0);
 
     for (size_t i = 0; i < dft.size(); ++i)
@@ -136,19 +202,16 @@ private:
     double quality;
     size_t size;
     std::vector<double> frequencies;
+    std::optional<std::pair<double, double>> window;
   };
-
-  qdft_config_t config;
 
   struct qdft_data_t
   {
     size_t period;
     size_t offset;
-    double weight;
+    F weight;
     std::complex<F> twiddle;
   };
-
-  std::vector<qdft_data_t> data;
 
   struct qdft_buffer_t
   {
@@ -156,6 +219,13 @@ private:
     std::vector<std::complex<F>> output;
   };
 
-  qdft_buffer_t buffer;
+  struct qdft_kernel_t
+  {
+    std::vector<qdft_data_t> data;
+    qdft_buffer_t buffer;
+  };
+
+  qdft_config_t config;
+  std::map<int, qdft_kernel_t> kernels;
 
 };
